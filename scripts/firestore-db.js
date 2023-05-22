991d10f72e2b5681e7e0a01f591fdbc09053b9f1
Revanthnayak <revanthtn_cs20.rvitm@rvei.edu.in>
2023-05-22 23:10:43 +0530

    authentication

---
+++ scripts/firestore-db.js
@@ -0,0 +1,39 @@
+const userDetails  = document.querySelector('.userDetails')
+
+function createUserCollection(user){
+   firebase.firestore().collection('users')
+   .doc(user.uid)
+   .set({
+       uid:user.uid,
+       name:user.displayName,
+       email:user.email,
+       phone:"",
+       specialty:"",
+       portfolioUrl:"",
+       experience:""
+   })
+}
+
+async function getuserInfo(userID){
+    if(userID){
+      const userInfoSnap = await  firebase.firestore()
+    .collection('users')
+    .doc(userID)
+    .get()
+
+   const userInfo = userInfoSnap.data()
+   if(userInfo){
+       userDetails.innerHTML = `
+       <h3>${userInfo.name}</h3>
+       <h3>${userInfo.email}</h3>
+       <h3>${userInfo.phone}</h3>
+       `
+   }    
+    }else{
+        userDetails.innerHTML = `
+        <h3>please login</h3>
+        `
+    }
+
+
+}
